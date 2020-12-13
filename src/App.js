f856a42d501b7585ebd92036b73f8dde07dbeb54
George Kruchinina <gkruchin@gmail.com>
2020-12-13 16:25:24 -0500

    updates to Details.js component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  //   return React.createElement("div", { id: "something-important" }, [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "Dog",
-  //       breed: "Havanese",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "Bird",
-  //       breed: "Cockatiel",
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Doink",
-  //       animal: "Cat",
-  //       breed: "Mixed",
-  //     }),
-  //   ]);
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id/" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
