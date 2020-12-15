51720aa4928bdffa89b39234684327445f07b9d7
George Kruchinina <gkruchin@gmail.com>
2020-12-14 20:58:23 -0500

    added modal and live functionality

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,10 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   // constructor(props) {
@@ -10,14 +14,16 @@ class Details extends React.Component {
   //     loading: true
   //   };
   // }
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   componentDidMount() {
+    //    throw new Error("rofl");
     pet
       .animal(this.props.id) //anything coming from the parent is "this.props" based - Read Only
       .then(({ animal }) => {
         //because we're returning a promise, we can use then
         this.setState({
+          url: animal.url,
           name: animal.name,
           animal: animal.type,
           location: `${animal.contact.address.city}, 
@@ -30,24 +36,62 @@ class Details extends React.Component {
       }, console.error);
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
   render() {
     if (this.state.loading) {
       return <h1>loading ...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal
+    } = this.state;
     return (
       <div className="details">
         <Carousel media={media} />
-        <div>
+        <div onClick={console.log}>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>
+                    No, I am a piece of shit apparently
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
